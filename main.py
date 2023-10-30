21a03b3756e37c15fd877a5fd705ee5f9cca657b
Giulio Vaccari <giulio09@marcov.it>
2023-10-30 11:57:51 +0100

    Code structure ok

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import gym
+import gymnasium as gym
+import numpy as np
+
 from controllers import PIDController
 
 def main():
     # Initialize environment
-    env = gym.make('Pendulum-v0')
+    env = gym.make('Pendulum-v1')
     
     # Initialize controller
     controller = PIDController(kp=1.0, ki=0.5, kd=0.1)
     
     # Simulation loop
-    state = env.reset()
+    state, info = env.reset()
     done = False
     while not done:
         env.render()
-        control_action = controller.control(state, reference=0)
-        state, reward, done, _ = env.step(control_action)
+        cos_theta, sin_theta, theta_dot = state
+        angle = np.arctan2(sin_theta, cos_theta)
+        control_action = controller.compute_action(angle, reference=0)
+        state, reward, done, truncated, _ = env.step([control_action])
+        env.close()
         
     env.close()
 
